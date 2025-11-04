d93efa9e56e16a68955bef5a6b69302caad3e70a
fokinikita <fokinikita@gmail.com>
2025-11-05 01:32:17 +0300

    fix GB pipeline

--- pipelines/run_gb_test.py
+++ pipelines/run_gb_test.py
@@ -47,7 +47,8 @@ def run_main() -> None:
                                 f" horizon {horizon} with avaliability {avaliability} was calculated,"
                                 f" features: {features_type}")
 
-    gb_pred_pl = pl.concat(gb_pred).write_csv('gb_pred_test.csv')
+    gb_pred_pl = pl.concat(gb_pred)
+    gb_pred_pl.write_csv('gb_pred_test.csv')
 
 if __name__ == "__main__":
     run_main()
