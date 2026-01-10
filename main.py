72ea7ccdedb9ebfb3a794505c996c26ba4853074
fokinikita <fokinikita@gmail.com>
2026-01-10 12:03:20 +0300

    add comments to main.py entrypoint

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from pipelines.run_ngb_test import run_main_ngb
 from pipelines.run_tabnet_test import run_main_tabnet
 
 if __name__ == "__main__":
-    run_main_gb()
-    run_main_ngb()
-    run_main_tabnet()
-    run_main_dfm()
+    run_main_gb() # считается быстро
+    run_main_ngb() # считается быстро
+    run_main_tabnet() # считается быстро
+    run_main_dfm() # считается долго, много опций гиперпараметров, можно не запускать без необходимости (закоментить)
     run_metrics(run_mfbvar=True)
